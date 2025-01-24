d08ee878dd0adca4a1231593b90bb20319dc50e8
pml-emerson <efranca@paradisemobile.com>
2025-01-23 22:36:54 -0300

    useBasket custom hook

---
+++ src/hooks/useBasket.ts
@@ -0,0 +1,73 @@
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { MenuItem, ModifierItem } from "../types";
+import {
+  addItem,
+  removeItem,
+  updateQuantity,
+} from "../features/basket/basketSlice";
+
+export const useBasket = () => {
+  const dispatch = useAppDispatch();
+  const { items } = useAppSelector((state) => state.basket);
+
+  const increaseQuantity = (itemId: number) => {
+    const item = items.find((item) => item.id === itemId);
+    if (item) {
+      dispatch(
+        updateQuantity({
+          id: itemId,
+          quantity: (item.quantity || 1) + 1,
+          selectedModifiers: item.selectedModifiers,
+        })
+      );
+    }
+  };
+
+  const decreaseQuantity = (itemId: number) => {
+    const item = items.find((item) => item.id === itemId);
+    if (item) {
+      const newQuantity = (item.quantity || 1) - 1;
+      if (newQuantity === 0) {
+        dispatch(
+          removeItem({ id: itemId, selectedModifiers: item.selectedModifiers })
+        );
+      } else {
+        dispatch(
+          updateQuantity({
+            id: itemId,
+            quantity: newQuantity,
+            selectedModifiers: item.selectedModifiers,
+          })
+        );
+      }
+    }
+  };
+
+  const addToBasket = (item: MenuItem, modifiers?: ModifierItem) => {
+    dispatch(addItem({ item, modifiers }));
+  };
+
+  const removeFromBasket = (id: number, selectedModifiers?: ModifierItem) => {
+    dispatch(removeItem({ id, selectedModifiers }));
+  };
+
+  const getItemQuantity = (itemId: number) => {
+    const item = items.find((item) => item.id === itemId);
+    return item?.quantity || 0;
+  };
+
+  const total = items.reduce(
+    (sum, item) => sum + item.price * (item?.quantity || 1),
+    0
+  );
+
+  return {
+    items,
+    total,
+    increaseQuantity,
+    decreaseQuantity,
+    addToBasket,
+    removeFromBasket,
+    getItemQuantity,
+  };
+};
