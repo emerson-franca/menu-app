d75d6899dce1091a0f05ab8f88ee19b99c6a4f5b
pml-emerson <efranca@paradisemobile.com>
2025-01-26 09:53:01 -0300

    update Home semantic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,62 @@
 import React, { useState } from "react";
 import { Menu } from "../components/Menu/Menu";
-import { Header, Hero } from "../components";
+import { Header, Hero, SearchBar } from "../components";
 import Basket from "../components/Basket/Basket";
+import { useMenu } from "../hooks/useMenu";
 
 const Home: React.FC = () => {
   const [isBasketVisible, setIsBasketVisible] = useState(false);
+  const { searchQuery, setSearchQuery } = useMenu();
 
   return (
-    <div className="min-h-screen bg-gray-50 w-full">
-      <Header />
-      <Hero />
-      <div className="relative md:container md:mx-auto md:px-4 md:py-8">
-        <div className="md:flex md:gap-8">
-          <div className="md:flex-1">
-            <Menu
-              isBasketVisible={isBasketVisible}
-              setIsBasketVisible={setIsBasketVisible}
-            />
+    <div className="min-h-screen bg-gray-100 w-full">
+      <header>
+        <Header />
+      </header>
+      <main>
+        <section aria-labelledby="hero-section">
+          <Hero />
+        </section>
+
+        <section aria-labelledby="search-section" className="bg-white">
+          <SearchBar
+            searchQuery={searchQuery}
+            setSearchQuery={setSearchQuery}
+          />
+        </section>
+
+        <section
+          aria-labelledby="menu-and-basket-section"
+          className="relative md:container md:mx-auto md:px-8 md:py-8 bg-gray-50 "
+        >
+          <div className="md:flex md:gap-8 bg-red">
+            <div className="md:flex-1">
+              <Menu
+                searchQuery={searchQuery}
+                isBasketVisible={isBasketVisible}
+                setIsBasketVisible={setIsBasketVisible}
+              />
+            </div>
+            <aside className="hidden md:block md:w-[350px]">
+              <div className="sticky top-8">
+                <Basket
+                  isOpen={isBasketVisible}
+                  onClose={() => setIsBasketVisible(false)}
+                />
+              </div>
+            </aside>
           </div>
-          <div className="hidden md:block md:w-[350px]">
-            <div className="sticky top-8">
+
+          <div className="md:hidden">
+            {isBasketVisible && (
               <Basket
                 isOpen={isBasketVisible}
                 onClose={() => setIsBasketVisible(false)}
               />
-            </div>
+            )}
           </div>
-        </div>
-
-        <div className="md:hidden">
-          {isBasketVisible && (
-            <Basket
-              isOpen={isBasketVisible}
-              onClose={() => setIsBasketVisible(false)}
-            />
-          )}
-        </div>
-      </div>
+        </section>
+      </main>
     </div>
   );
 };
